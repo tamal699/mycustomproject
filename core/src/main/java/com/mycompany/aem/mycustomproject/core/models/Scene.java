e5e870f1893d21213a0119f2e137f80cf19adb57
tamal699 <tamal.kundu@gds.ey.com>
2025-09-26 15:57:45 +0530

    some chnages

--- core/src/main/java/com/mycompany/aem/mycustomproject/core/models/Scene.java
+++ core/src/main/java/com/mycompany/aem/mycustomproject/core/models/Scene.java
@@ -1,4 +1,26 @@
 package com.mycompany.aem.mycustomproject.core.models;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.*;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Scene {
+
+    @Inject
+    private String placeName;
+    @Inject private String description;
+    @Inject private String location;
+    @ValueMapValue(name = "fileReference")
+    private String sceneryImage;
+
+    public String getPlaceName() { return placeName; }
+    public String getDescription() { return description; }
+    public String getLocation() { return location; }
+    public String getSceneryImage() {
+        return sceneryImage;
+    }
+
 }
--- core/src/main/java/com/mycompany/aem/mycustomproject/core/models/ScenicHighlightsModel.java
+++ core/src/main/java/com/mycompany/aem/mycustomproject/core/models/ScenicHighlightsModel.java
@@ -1,4 +1,20 @@
 package com.mycompany.aem.mycustomproject.core.models;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.*;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ScenicHighlightsModel {
+
+    @Inject
+    private List<Scene> scenes;
+
+    public List<Scene> getScenes() {
+        return scenes;
+    }
 }
